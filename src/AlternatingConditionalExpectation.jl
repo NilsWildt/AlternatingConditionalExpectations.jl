114d6483f47dfa4dfa4f34374acf07c8c5f579cc
nildt <mail@nwildt.de>
2021-02-11 16:58:57 +0100

    buggy

--- src/AlternatingConditionalExpectation.jl
+++ src/AlternatingConditionalExpectation.jl
@@ -227,10 +227,11 @@ function ACE_bivariate(myace::Acerun)
 
     @inbounds while abserr > errorbound &&  i < itermax_outer #  || i < 5
         j = 0
-          Φ_x = 0. .*Φ_x # Set back to zero (fresh start versoin.)
+        #   Φ_x = 0. .*Φ_x # Set back to zero (fresh start versoin.)
+        # Without, it's the restart version.
         while abserr > errorbound &&  j < itermax_inner 
             err_old = err_new
-            Φ_1 = cond_exp(Φ_x, Θ_y,  myace,  sIx, bsIx) # E_y(...)
+            Φ_1 = cond_exp(X, Θ_y.-Φ_x,  myace,  sIx, bsIx) # E_y(...)
             Φ_x =   stoch_normalize(Φ_1) #  Φ_1  .- mean(Φ_1) # normalize mean #   stoch_normalize(Φ_1)# stoch_normalize(Φ_1)
 
             err_new = unexplained_variance(Φ_x, Θ_y)
