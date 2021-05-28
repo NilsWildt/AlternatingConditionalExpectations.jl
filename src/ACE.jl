948bcd5240c9fce38f56d9435f581cf3534c10ed
nildt <mail@nwildt.de>
2021-05-28 16:43:48 +0200

    Not sure if stupid.

--- src/ACE.jl
+++ src/ACE.jl
@@ -173,129 +173,8 @@ end
     return (X  .- tmpmean) ./ std(X; corrected = true, mean = tmpmean)
 end
 
-  # Multivariate ACE values
-  # Restart version
-# function ACEmulti_obsolete(myace::ACEsim{T,S})where {T,S <: AbstractArray} 
-#     @info "Startin multivariate ACE, the predictor variables have a size of $(size(myace.X))"
-#     # Normalize Mean an Variance, save the transformation
-#     X = myace.X # Predictor variables
-#     @show Nx, m_parameter = size(X)
-#     Y = myace.Y # Response VEctor
-
-#     # Sortindex of all predcitors
-#     sIx = Array{Int64}(undef, Nx, m_parameter)
-#     bsIx = Array{Int64}(undef, Nx, m_parameter)
-#     for i in 1:m_parameter
-#         @debug "Whatsup" Vector{Float64,1}(X[:,i])
-#         sIx[:,i], bsIx[:,i] = get_sortidx(Array{Float64,1}(@view X[:,i]))
-#     end
-#     # SOrt response variables
-#     sIy, bsIy = get_sortidx(Y)
-#         # Preallocate
-#     Θ_y = stoch_normalize(Y)
-#     Θ_1 = Θ_y
-#     Φ_1 = zeros(Float64, Nx, m_parameter)
-#     Φ_x = zeros(Float64, Nx, m_parameter) # Start with zeros
-
-#     err_candidate = 1.E6
-#     err_new = 0.0
-
-#     abserr = err_candidate
-#     errorbound =    myace.errorbound 
-        
-#     i = 0 # Counter outer loop
-#     itermax_outer = myace.itermax_outer
-#     itermax_inner = myace.itermax_inner
-
-#     itercount_inner = 0
-#     itercount_outer = 0
-
-#     @inbounds while abserr > errorbound &&  i < itermax_outer #  || i < 5
-#         j = 0 # Counter inner loop
-#         while abserr > errorbound &&  j < itermax_inner
-#             j = j + 1 # Count immediately (as indexing starts at 1)
-#             @inbounds @simd for k in 1:m_parameter 
-#                 Φ_1[:,k] = 𝔼_conditional(X, Θ_y,  myace,  sIx, bsIx) # E_y(...)
-#                 Φ_x[:,k] =   stoch_normalize(Φ_1[:,k]) #  Φ_1  .- mean(Φ_1) # normalize mean #   stoch_normalize(Φ_1)# stoch_normalize(Φ_1)
-#             end
-#             err_new = ε²(Φ_x, Θ_y)
-#             abserr = abs(err_new - err_candidate) 
-#             itercount_inner = itercount_inner + 1
-#         end
-#         i += 1 # Count immediately (as indexing starts at 1)
-
-#         err_candidate = err_new
-
-#         Θ_1 =  𝔼_conditional(Y, Φ_x, myace, sIy, bsIy) # E_x(Phi(x)|Y)
-#         Θ_y =  stoch_normalize(Θ_1) # Θ_1 .- mean(Θ_1) #
-#         # Θ_y  = Θ_1
-#         err_new = ε²(Φ_x, Θ_y)
-#         abserr = abs(err_new - err_candidate) 
-#                 # println("In iter $i we get an error of $  abserr to the loop before.")
-#                 # printfmt("In Iteration $i we get an error of {:.9f}",abs(err_new - err_candidate))
-#         # @info "ACE Loop:" itercount_outer
-#         itercount_outer = itercount_outer + 1
-#     end
-#     # println("Did $i iterations!")
-#             # p = ACE_4_plot(X, Y, Φ_x, Θ_y)
-#             # display(p)
-#             # @info "Dbg" Θ_y Φ_x
-  
-
-#     var_unexp = ε²(Φ_x, Θ_y)
-#     thisemse = MSE(Φ_x, Θ_y)    
-#     correl = cor(Φ_x, Θ_y)
-#     itercount_total = itercount_outer * itercount_inner
-#     return  Φ_x, Θ_y, itercount_inner, itercount_outer, itercount_total, var_unexp, thisemse, correl
-# end
-
-# function ACE_bivariate_basic(myace::ACEsim{T,S})where {T,S <: AbstractArray} 
-#     Nx = length(myace.X)
-#     X = myace.X
-#     Y = myace.Y
-#     sIx, bsIx = get_sortidx(vec(X))
-#     sIy, bsIy = get_sortidx(vec(Y))
-#     Θ_y = stoch_normalize(Y)
-#     Θ_candidate = copy(Θ_y)
-#     Θ_1 = copy(Θ_y)
-#     Φ_1 = stoch_normalize(X)
-#     Φ_x = stoch_normalize(X)
-#     @debug "What is Φₓ?" typeof(Φ_x)
-#     Φ_candidate = copy(Φ_x)
-#         # @debug "Check sort X:" issorted(Φ_1) issorted(Φ_x) issorted(Φ_candidate) 
-#         # @debug "Check sort Y:" issorted(Θ_1) issorted(Θ_y) issorted(Θ_candidate) 
-#     @debug "Before the loop." ε²(Θ_candidate, Φ_candidate)  ε²(Θ_y, Φ_x)
-
-#     i = 0
-#     #  h1 = Plots.scatter(Φ_x,Θ_y)
-#     conv_err = []
-#     @inbounds while ((abs(ε²(Θ_y, Φ_x) - ε²(Θ_candidate, Φ_candidate)) > myace.errorbound) || i < 2 ) && i < myace.itermax_outer
-#         push!(conv_err, ε²(Θ_y, Φ_x))
-#         Φ_candidate = Φ_x
-#         Θ_candidate = Θ_y
-#         Φ_x = 𝔼_conditional(Θ_y, X, myace, sIx, bsIx)
-#         Θ_1 = 𝔼_conditional(Φ_x, Y, myace, sIy, bsIy) 
-#         Θ_y = stoch_normalize(Θ_1)
-#         @debug "Iteration" i  ε²(Θ_candidate, Φ_candidate) - ε²(Θ_y, Φ_x)
-#         #   if mod(i,2)==0 
-#             # Plots.scatter!(h1,Φ_candidate,Θ_candidate[bsIy],s=1,alpha=1/(i+1))
-#         #   end
-#         i += 1
-#     end
-#     # PyPlot.scatter(collect(1:length(conv_err)),log.(conv_err))
-#     # gcf()
-#     Φ_x = Φ_candidate  # (candidate was successful)
-#     Θ_y = Θ_candidate
-
-# #   var_unexp = ε²(Φ_x, Θ_y)
-# #     thisemse = MSE(Φ_x, Θ_y)
-# #     correl = cor(Φ_x, Θ_y)
-#     # itercount_total = itercount_outer * itercount_inner
-#     return  Φ_x, Θ_y, sIx, sIy, conv_err
-# end
-
-@inline function sum_without(X::AbstractArray, k::Int64)
-    @fastmath return sum(hcat(X[:,1:k - 1], X[:,k + 1:end]), dims = 2)
+function sum_without(X::AbstractArray, k::Int64)
+    @fastmath return sum(X[:,1:k - 1], dims = 2) + sum(X[:,k + 1:end], dims = 2)
 end
 
 function run(myace::ACEsim{T,S}) where {T,S <: AbstractArray} 
@@ -334,27 +213,29 @@ function run(myace::ACEsim{T,S}) where {T,S <: AbstractArray}
     while (abs(e_old - e_new) > myace.errorbound || i < 2)  && i <= myace.itermax_outer
         j = 1
         while (abs(e_old - e_new) > myace.errorbound || j < 2) && j <= myace.itermax_inner
-            Φ_x = Φ_candidate 
             e_old = e_new
             @inbounds for k in 1:m_parameter
                 Φ_candidate[:,k] = 𝔼_conditional(Θ_y .- sum_without(Φ_candidate, k), X,  myace,  sIx[:,k], bsIx[:,k]) # E_y(...)
                 Φ_candidate[:,k] = Φ_candidate[:,k] .- mean(Φ_candidate[:,k])
             end
+            Φ_x = Φ_candidate 
             e_new =  ε²(Θ_y, Φ_candidate)
             @debug "Inner loop" (e_old - e_new )  i j
             push!(conv_err, abs(e_old - e_new))
             j += 1
             totalcount  += 1
         end
+        Φ_x = Φ_candidate  # Not sure, if I want this, depends on the error I suppose.
         e_old = e_new;
-        Θ_y .=  Θ_candidate
         Θ_candidate  = 𝔼_conditional(sum(Φ_x, dims = 2), Y, myace, sIy, bsIy) 
         Θ_candidate  = stoch_normalize(Θ_candidate)
+        Θ_y .=  Θ_candidate
         i += 1
         e_new =  ε²(Θ_candidate, Φ_x)
         @debug "outer loop" (e_old - e_new ) i j
         push!(conv_err, abs(e_old - e_new))
     end
+      Θ_y .=  Θ_candidate
 
     r_orig =cor(X, Y)
     r² = cor(Φ_x, Θ_y)
@@ -384,9 +265,9 @@ end
            # # set up the subplots
             link --> :none
             size-->plotsize
-  xguide --> "x"
-    yguide --> "y"
-                    margin -->20Plots.px
+            xguide --> "x"
+            yguide --> "y"
+            margin -->20Plots.px
 
         if full
             X = bf.X
