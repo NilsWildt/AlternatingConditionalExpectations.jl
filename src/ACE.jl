2377c0ab5cfccf457e7e1df47cae77fa67c1c300
nildt <mail@nwildt.de>
2021-06-02 16:16:30 +0200

    fixes

--- src/ACE.jl
+++ src/ACE.jl
@@ -213,13 +213,14 @@ function run(myace::ACEsim{T,S}) where {T,S <: AbstractArray}
     i = 1
     while (abs(e_old - e_new) > myace.errorbound)  && i <= myace.itermax_outer
         j = 1
+        Θ_y =  Θ_candidate
         while (abs(e_old - e_new) > myace.errorbound ) && j <= myace.itermax_inner
             e_old = e_new
             if myace.multiloopversion==:fresh
                 Φ_candidate .= 0.0 .* Φ_candidate
             end
             @inbounds for k in 1:m_parameter
-                Φ_candidate[:,k] = 𝔼_conditional(Θ_candidate .- sum_without(Φ_candidate, k), X,  myace,  sIx[:,k], bsIx[:,k]) # E_y(...)
+                Φ_candidate[:,k] = 𝔼_conditional(Θ_y .- sum_without(Φ_candidate, k), X,  myace,  sIx[:,k], bsIx[:,k]) # E_y(...)
                 Φ_candidate[:,k] = Φ_candidate[:,k] .- mean(Φ_candidate[:,k])
            end
             e_new =  ε²(Θ_y, Φ_candidate)
@@ -237,7 +238,6 @@ function run(myace::ACEsim{T,S}) where {T,S <: AbstractArray}
         @debug "outer loop" (e_old - e_new ) i j
         push!(conv_err, abs(e_old - e_new))
     end
-    Θ_y =  Θ_candidate
     r_orig =cor(X, Y)
     r² = cor(Φ_x, Θ_y)
     # spearman_orig = StatsBase.corspearman(vec(X), vec(Y))
