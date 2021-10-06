a135c8ce2709ad4a4ccef28febc2e569d334622d
NilsWildt <mail@nwildt.de>
2021-10-06 12:10:37 +0200

    fixes and trys

--- scripts/demo3d.jl
+++ scripts/demo3d.jl
@@ -20,34 +20,35 @@ Nk = 500
 # # Benchmark =  f_toy(Nk, 1, "normal", 1.0, 0.0, false, 0, false, (-12., 12.))
 # m =  Benchmark.X
 # X = hcat(m,Benchmark2.X)
-rng =  MersenneTwister(42)    
+rng =  MersenneTwister(2)    
  μ = 0.0
 X1 =      randn(rng,  Float64, (Nk,1)) .+ μ
-lb = -3
-ub = 3.0
+lb = -5
+ub = 5.0
 X2 = abs(ub - lb) .* (rand(rng,  Float64, (Nk,1))) .+ lb
-lb = 0
-ub = 0.5
+lb = -5
+ub = 5
 X3 = abs(ub - lb) .* (rand(rng,  Float64, (Nk,1))) .+ lb
-X = hcat(X3,X1,X3)
-Y =  @. sin.(2*X1) + X2 + log.(X3)  
-Y = Y .+ 0.1.*randn(rng,  Float64, (Nk,1))
+X = hcat(X1,X2,X3)
+Y =  @.X2.^2+sin.(X3) + X1
+Y = Y .+ 1.0  .*randn(rng,  Float64, (Nk,1))
 # mysmoother1 = ACE.LAS(Nk ÷ 2)
 mysmoother2 = ACE.LASb(Nk ÷ 5)
-mysmoother3 =  ACE.LLSS(Nk ÷ 3)
+mysmoother3 =  ACE.LLSS(Nk ÷ 6)
 mysmoother4 =  ACE.LLSSb(Nk ÷ 4)
 mysmoother5 = ACE.FRSS([0.05,0.1,0.5], 0.2, 0.2)
-σ = 0.4
+σ =1.3
 mykernel = ACE.Kernelregression.Gaussian(σ)
-reg = 1.0
+reg = 0.003
 mysmoother6 = ACE.NWKernelsmooth(mykernel)
-mysmoothers = Array{ACE.Smoother}([mysmoother2])
+mysmoothers = Array{ACE.Smoother}([mysmoother6])
 # guess_parameters!(mysmoother, Nk)
 myace =  ACE.ACEsim(X,Y,mysmoothers)
 res = ACE.run(myace)
 # Benchmark.Φ_x, Benchmark.Θ_y, Benchmark.sIx, Benchmark.sIy, Benchmark.conv_err
 # h1 =  benchmark_ace_plot(Benchmark, "ACE Demo", 80) 
-gr()
+plotly()
+# gr()
 h1 = plot(res)
 display(h1)
 
